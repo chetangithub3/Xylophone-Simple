b0913eccebfa0e87a77b3e583e9ecac8a1fb1866
Chetan <chetan.getsmail@gmail.com>
2022-02-25 19:36:21 -0500

    Added animation

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var player: AVAudioPlayer?
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+       
+        playNote(title: sender.currentTitle?.uppercased() ?? "D")
+        
+       
+    }
+    
+    func animateButton(sender: UIButton){
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+           // Code you want to be delayed
+            sender.alpha = 0.5
+        }
+    }
+    
+    func playNote(title: String){
+        
+        
+        
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")!
+
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                guard let player = player else { return }
+
+                player.prepareToPlay()
+                player.play()
+
+            } catch let error as NSError {
+                print(error.description)
+            }
+        print(title)
+    }
 
 }
 
